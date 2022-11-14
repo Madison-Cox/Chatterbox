5deb9cbb60c3d043a9ae9fc79480589345e3fa1b
Madison-Cox <madison.cox5285@hotmail.com>
2022-11-14 10:12:42 -0600

    added giftedchat

--- components/Chat.js
+++ components/Chat.js
@@ -1,20 +1,69 @@
+import { GiftedChat } from 'react-native-gifted-chat'
 import React from 'react';
-import { View, Text, StyleSheet, Title } from 'react-native';
-
+import { View, Text, StyleSheet, Title, KeyboardAvoidingView } from 'react-native';
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
 
   componentDidMount() {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
   }
 
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
 
   render() {
     let color = this.props.route.params.color;
+    {
+      Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
+    }
     return (
-      <View style={{ backgroundColor: color, flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Chatterbox</Text>
+      <View style={{ backgroundColor: color, flex: 1 }}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
       </View>
-    )
-  }
+    );
+  };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  chatTitle: {
+    color: '#FFFFFF'
+  }
+})
